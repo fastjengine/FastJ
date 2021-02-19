4c6ea9882e81685714f7d66cd80649b68e54092f
lucasstarsz <andrewrcdey@gmail.com>
2021-02-19 02:10:43 -0500

    Fixed module-info to export correct packages

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,9 @@
 /**
  * The FastJ Game Library in its entirety.
  * <p>
- * This game library is split into two main parts: the framework with which to build games, and the engine running it.
- * The library does not use any external dependencies -- it relies only on Java 15's {@code java.desktop} module, mainly
- * using AWT and Swing to display output.
+ * This game library is split into two main parts: the framework with which to build games (every package except {@code
+ * engine}), and the engine running it (just package {@code engine}). FastJ does not use any external dependencies -- it
+ * relies entirely on Java 15's {@code java.desktop} module, using AWT and Swing to display output.
  * <p>
  * For more information, check out
  * <a href="https://github.com/lucasstarsz/FastJ-Engine" target="_blank">the github repository.</a>
@@ -13,25 +13,22 @@ module fastj.library {
 
     exports io.github.lucasstarsz.fastj.engine;
 
-    exports io.github.lucasstarsz.fastj.framework;
-
-    exports io.github.lucasstarsz.fastj.render;
-    exports io.github.lucasstarsz.fastj.render.util;
-
     exports io.github.lucasstarsz.fastj.graphics;
     exports io.github.lucasstarsz.fastj.graphics.shapes;
     exports io.github.lucasstarsz.fastj.graphics.text;
-
-    exports io.github.lucasstarsz.fastj.ui;
-    exports io.github.lucasstarsz.fastj.ui.elements;
 
     exports io.github.lucasstarsz.fastj.io.keyboard;
     exports io.github.lucasstarsz.fastj.io.mouse;
 
     exports io.github.lucasstarsz.fastj.math;
 
-    exports io.github.lucasstarsz.fastj.systems.game;
+    exports io.github.lucasstarsz.fastj.render;
+
     exports io.github.lucasstarsz.fastj.systems.behaviors;
+    exports io.github.lucasstarsz.fastj.systems.game;
     exports io.github.lucasstarsz.fastj.systems.tags;
     exports io.github.lucasstarsz.fastj.systems.input;
+
+    exports io.github.lucasstarsz.fastj.ui;
+    exports io.github.lucasstarsz.fastj.ui.elements;
 }
