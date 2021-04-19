0055b58ff5cf5d1f5096266d4bf78436785c4c5c
Yeff <76024824+YeffyCodeGit@users.noreply.github.com>
2021-04-19 22:10:10 +0600

    Yet another fix

--- src/test/java/unittest/testcases/math/MathsTests.java
+++ src/test/java/unittest/testcases/math/MathsTests.java
@@ -121,7 +121,7 @@ public class MathsTests {
 
     @Test
     public void checkLerpValues() {
-        assertEquals(5, Maths.lerp(5, 2, 0), "The lerped value should be 5f.");
-        assertEquals(-1, Maths.lerp(5, 2, 2), "The lerped value should be -1f.");
+        assertEquals(5, Maths.lerp(5f, 2f, 0f), "The lerped value should be 5f.");
+        assertEquals(-1, Maths.lerp(5f, 2f, 2f), "The lerped value should be -1f.");
     }
 }
