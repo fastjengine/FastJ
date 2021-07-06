c58f679654980860eba43d7142dc16e78e7072f1
lucasstarsz <andrewrcdey@gmail.com>
2021-07-05 22:15:32 -0400

    Update branch with content from origin/main

--- src/main/java/tech/fastj/engine/FastJEngine.java
+++ src/main/java/tech/fastj/engine/FastJEngine.java
@@ -7,6 +7,7 @@ import tech.fastj.graphics.display.Display;
 import tech.fastj.graphics.util.DisplayUtil;
 
 import tech.fastj.systems.audio.AudioManager;
+import tech.fastj.systems.audio.AudioPlayer;
 import tech.fastj.systems.behaviors.BehaviorManager;
 import tech.fastj.systems.control.LogicManager;
 import tech.fastj.systems.tags.TagManager;
@@ -487,6 +488,7 @@ public class FastJEngine {
         Mouse.stop();
         Keyboard.stop();
         AudioManager.reset();
+        AudioPlayer.reset();
         BehaviorManager.reset();
         TagManager.reset();
 
