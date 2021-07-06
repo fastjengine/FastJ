c6cf2a54fd6f780184ab8df25a594326f9404c69
lucasstarsz <andrewrcdey@gmail.com>
2021-07-05 21:13:40 -0400

    (#38) Updated model read/write example

--- src/example/java/tech/fastj/example/modelreadwrite/Main.java
+++ src/example/java/tech/fastj/example/modelreadwrite/Main.java
@@ -5,7 +5,7 @@ import tech.fastj.math.Pointf;
 import tech.fastj.graphics.game.Model2D;
 import tech.fastj.graphics.game.Polygon2D;
 import tech.fastj.graphics.util.DrawUtil;
-import tech.fastj.graphics.util.PsdfUtil;
+import tech.fastj.graphics.util.ModelUtil;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -23,13 +23,13 @@ public class Main {
 
         // We'll start with the model code from the "model2d" example.
         Pointf[] smallSquareMesh1 = DrawUtil.createBox(0f, 0f, 50f);
-        Polygon2D smallSquare1 = new Polygon2D(smallSquareMesh1);
+        Polygon2D smallSquare1 = Polygon2D.fromPoints(smallSquareMesh1);
 
         Pointf[] smallSquareMesh2 = DrawUtil.createBox(50f, 50f, 25f);
-        Polygon2D smallSquare2 = new Polygon2D(smallSquareMesh2);
+        Polygon2D smallSquare2 = Polygon2D.fromPoints(smallSquareMesh2);
 
         Polygon2D[] smallSquaresArray = {smallSquare1, smallSquare2};
-        Model2D smallSquares = new Model2D(smallSquaresArray);
+        Model2D smallSquares = Model2D.fromPolygons(smallSquaresArray);
 
 
         /* Now that that's out of the way, let's write this Model2D to a file.
@@ -44,17 +44,18 @@ public class Main {
 
 
         /* This is the path which we will be writing to. It resolves to
-         * "src/example/resources/modelreadwrite/smallSquares.psdf". */
-        String smallSquaresFilePath = "src/example/resources/modelreadwrite/smallSquares.psdf";
+         * "src/example/resources/smallSquares.psdf". */
+        Path smallSquaresFilePath = Path.of("src/example/resources/smallSquares.psdf");
 
         // And now, we can call the model-writing method.
-        PsdfUtil.writePsdf(smallSquaresFilePath, smallSquares);
+        // We can write the model using ModelUtil.writeModel(path, model).
+        ModelUtil.writeModel(smallSquaresFilePath, smallSquares);
 
 
-        /* As a small aside, this gets the file and prints out its contents.
+        /* As a small aside, this gets the file we just wrote to and prints out its contents.
          * The code is not the primary focus of the example -- this is just to give a visual. */
         try {
-            String smallSquaresFileContents = Files.readString(Path.of(smallSquaresFilePath));
+            String smallSquaresFileContents = Files.readString(smallSquaresFilePath);
             FastJEngine.log("Contents of smallSquares.psdf: \n" + smallSquaresFileContents);
         } catch (IOException exception) {
             exception.printStackTrace();
@@ -63,22 +64,28 @@ public class Main {
 
         /* Next up, we'll read the file back.
          * Since we already have the string denoting the location of the file, we can reuse it. */
-        Polygon2D[] smallSquaresArrayFromFile = PsdfUtil.loadPsdf(smallSquaresFilePath);
 
-        // We can use this to create a new Model2D.
-        Model2D smallSquaresFromFile = new Model2D(smallSquaresArrayFromFile);
+        // We can read the model from the file using ModelUtil.loadModel(path).
+        Polygon2D[] smallSquaresArrayFromFile = ModelUtil.loadModel(smallSquaresFilePath);
+
+        // This nets us a Polygon2D array -- we can use this to create a new Model2D.
+        Model2D smallSquaresFromFile = Model2D.fromPolygons(smallSquaresArrayFromFile);
 
         /* Here, we check for equality between the original and the read file.
          * As expected, the two contain the exact same contents. */
-        FastJEngine.log("smallSquaresfromFile == smallSquares? " + smallSquaresFromFile.equals(smallSquares));
+        FastJEngine.log("smallSquaresFromFile == smallSquares? " + smallSquaresFromFile.equals(smallSquares));
 
 
         /* Another small aside: this removes the file we created earlier in the program.
          * This code is not the primary focus of the example -- it's just to clean up the work we did. */
         try {
-            boolean wasDeleted = Files.deleteIfExists(Path.of(smallSquaresFilePath));
+            boolean wasDeleted = Files.deleteIfExists(smallSquaresFilePath);
             if (!wasDeleted) {
-                FastJEngine.warning("The file at \"" + smallSquaresFilePath + "\" was not deleted.");
+                FastJEngine.warning(
+                        "The file at \"" + smallSquaresFilePath.toAbsolutePath() + "\" was not deleted. Whoever is reading this, should go delete that file."
+                                + System.lineSeparator()
+                                + "Then, report this error as an issue in the engine repository."
+                );
             }
         } catch (IOException exception) {
             exception.printStackTrace();
