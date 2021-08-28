0480ce36a154665a2a475a00f3d71d5618b2a5a9
lucasstarsz <andrewrcdey@gmail.com>
2021-08-28 10:37:00 -0400

    change file extension to return empty string when no extension is given, rather than the original path

--- src/main/java/tech/fastj/resources/files/FileUtil.java
+++ src/main/java/tech/fastj/resources/files/FileUtil.java
@@ -15,11 +15,16 @@ public class FileUtil {
      * <p>
      * This method does <b>not</b> account for file extensions with more than one dot ({@code .}) -- in cases like
      * those, only the last part of the extension will be returned.
+     * <p>
+     * For paths which contain no file extension, an empty {@code String} will be returned.
      *
      * @param filePath The {@code Path} to get the file extension of.
      * @return The {@code Path}'s file extension.
      */
     public static String getFileExtension(Path filePath) {
-        return filePath.toString().substring(filePath.toString().lastIndexOf(".") + 1);
+        if (filePath.toString().contains(".")) {
+            return filePath.toString().substring(filePath.toString().lastIndexOf(".") + 1);
+        }
+        return "";
     }
 }
