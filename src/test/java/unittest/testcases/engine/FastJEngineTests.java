6e1ede68c31e93dc7e597e858cc88f948954c952
lucasstarsz <andrewrcdey@gmail.com>
2021-10-18 23:31:44 -0400

    fix concurrency issues with FastJEngineTests using method orders and waiting after engine closing

--- src/test/java/unittest/testcases/engine/FastJEngineTests.java
+++ src/test/java/unittest/testcases/engine/FastJEngineTests.java
@@ -5,15 +5,21 @@ import tech.fastj.graphics.display.Display;
 
 import tech.fastj.systems.control.SimpleManager;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import unittest.EnvironmentHelper;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assumptions.assumeFalse;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class FastJEngineTests {
 
     @BeforeAll
@@ -22,15 +28,13 @@ class FastJEngineTests {
     }
 
     @Test
+    @Order(0)
     void checkRunAfterUpdate() {
         AtomicBoolean ranAfterUpdate = new AtomicBoolean();
         FastJEngine.init("yeet", new SimpleManager() {
             @Override
             public void init(Display display) {
-                FastJEngine.runAfterUpdate(() -> {
-                    ranAfterUpdate.set(true);
-                    FastJEngine.forceCloseGame();
-                });
+                FastJEngine.runAfterUpdate(() -> ranAfterUpdate.set(true));
             }
 
             @Override
@@ -41,21 +45,21 @@ class FastJEngineTests {
             public void render(Display display) {
             }
         });
+
+        Executors.newSingleThreadScheduledExecutor().schedule(FastJEngine::forceCloseGame, 1, TimeUnit.SECONDS);
         FastJEngine.run();
 
         assertTrue(ranAfterUpdate.get(), "After one update completes, the ranAfterUpdate boolean should have been set to true.");
     }
 
     @Test
+    @Order(1)
     void checkRunAfterRender() {
         AtomicBoolean ranAfterRender = new AtomicBoolean();
         FastJEngine.init("yeet", new SimpleManager() {
             @Override
             public void init(Display display) {
-                FastJEngine.runAfterRender(() -> {
-                    ranAfterRender.set(true);
-                    FastJEngine.forceCloseGame();
-                });
+                FastJEngine.runAfterRender(() -> ranAfterRender.set(true));
             }
 
             @Override
@@ -66,6 +70,8 @@ class FastJEngineTests {
             public void render(Display display) {
             }
         });
+
+        Executors.newSingleThreadScheduledExecutor().schedule(FastJEngine::forceCloseGame, 1, TimeUnit.SECONDS);
         FastJEngine.run();
 
         assertTrue(ranAfterRender.get(), "After one render completes, the ranAfterRender boolean should have been set to true.");
