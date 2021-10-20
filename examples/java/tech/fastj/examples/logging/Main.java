9cf92f59f91c1e943d20fb111994d51ed1bca88c
lucasstarsz <andrewrcdey@gmail.com>
2021-10-20 16:28:03 -0400

    (#4) work in progress -- updating logging example

--- examples/java/tech/fastj/examples/logging/Main.java
+++ examples/java/tech/fastj/examples/logging/Main.java
@@ -6,26 +6,28 @@ public class Main {
     public static void main(String[] args) {
         /* FastJ Logging */
 
-        /* FastJ's logging solution is not very robust just yet. It currently makes use of the
-         * standard out/err streams (System.out and System.err). While not performant, it is currently sufficient for the engine's usage. */
+        // TODO: Finish update and amend last commit
 
-        /* First up, "FastJEngine#log". This method prints out in this style:
-         * "INFO: <your message here, without the braces>" */
+        /* FastJEngine makes use of SLF4J (Simple Logging Facade for Java) for its logging solution.
+         * This allows the user to use a specific logging framework of their choice -- if such a choice isn't needed,
+         * FastJ templates will include SimpleLogger as a dependency.
+         * Otherwise, you may choose whichever logging framework you like, so long as it supports SLF4J! */
+
+        /* First up, "FastJEngine#log". The engine's LogLevel has to be set to "LogLevel.Info" or higher for this to
+         * print out. */
 
         FastJEngine.log("Hello! This is an informational logging statement.");
 
-        /* Next, "FastJEngine#warning". This method prints out in this style:
-         * "WARNING: <your message here, without the braces>" */
+        /* Next, "FastJEngine#warning". The engine's LogLevel has to be set to "LogLevel.Warn" or higher for this to
+         * print out. */
 
         FastJEngine.warning("Be careful now! This is a warning.");
 
-        /* Lastly, "FastJEngine#error". This method is special, because it not only prints out the
-         * error -- it crashes the engine with an exception as well.
-         * This method prints in this style:
-         * "ERROR: <your message here, without the braces>"
-         * "Exception <the rest of the stack trace would be here>" */
-
-        /* Under normal circumstances, this method also requires that you add an exception -- the
+        /* Lastly, "FastJEngine#error". This method not only prints force closes the game and prints out the error, but
+         * it crashes the engine with an exception as well. The engine's LogLevel has to be set to "LogLevel.Error" or
+         * higher for this to print out.
+         *
+         * Under normal circumstances, this method also requires that you add an exception -- the
          * exception that caused a need to call "FastJEngine#error".
          * For the sake of demonstration, we will create an exception ourselves. */
         IllegalStateException exampleException = new IllegalStateException("This is an example exception.");
