ce67e44c75745c7ab6a14e763298849a3455054f
lucasstarsz <andrewrcdey@gmail.com>
2021-10-23 18:16:47 -0400

    add engine init unit test

--- src/test/java/unittest/testcases/engine/FastJEngineTests.java
+++ src/test/java/unittest/testcases/engine/FastJEngineTests.java
@@ -1,11 +1,14 @@
 package unittest.testcases.engine;
 
 import tech.fastj.engine.FastJEngine;
+import tech.fastj.engine.HWAccel;
+import tech.fastj.engine.config.EngineConfig;
 import tech.fastj.engine.config.ExceptionAction;
 import tech.fastj.graphics.display.Display;
 
 import tech.fastj.systems.control.SimpleManager;
 
+import java.util.UUID;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.junit.jupiter.api.BeforeAll;
@@ -14,7 +17,9 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import unittest.EnvironmentHelper;
+import unittest.mock.systems.control.MockEmptySimpleManager;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assumptions.assumeFalse;
 
@@ -80,5 +85,22 @@ class FastJEngineTests {
         FastJEngine.run();
 
         assertTrue(ranAfterRender.get(), "After one render completes, the ranAfterRender boolean should have been set to true.");
+    }
+
+    @Test
+    @Order(2)
+    void checkEngineInitConfig_shouldMatchDefault() {
+        String gameTitle = UUID.randomUUID().toString();
+
+        FastJEngine.init(gameTitle, new MockEmptySimpleManager());
+
+        assertEquals(gameTitle, FastJEngine.getDisplay().getTitle(), "The display title should match the original title.");
+        assertEquals(EngineConfig.Default.targetFPS(), FastJEngine.getTargetFPS(), "The engine config FPS should match the default FPS.");
+        assertEquals(EngineConfig.Default.targetUPS(), FastJEngine.getTargetUPS(), "The engine config UPS should match the default UPS.");
+        assertEquals(EngineConfig.Default.windowResolution(), FastJEngine.getDisplay().getWindowResolution(), "The engine config window resolution should match the default window resolution.");
+        assertEquals(EngineConfig.Default.internalResolution(), FastJEngine.getDisplay().getInternalResolution(), "The engine config internal resolution should match the default internal resolution.");
+        assertEquals(EngineConfig.Default.hardwareAcceleration(), FastJEngine.getHardwareAcceleration(), "The engine config hardware acceleration should match the default hardware acceleration.");
+        assertEquals(EngineConfig.Default.exceptionAction(), FastJEngine.getExceptionAction(), "The engine config exception action should match the default exception action.");
+        assertEquals(EngineConfig.Default.logLevel(), FastJEngine.getLogLevel(), "The engine config log level should match the default log level.");
     }
 }
