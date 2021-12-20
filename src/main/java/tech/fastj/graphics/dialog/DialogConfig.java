6133772f3c877248bd7f48c0effb0e7bad3aa09b
lucasstarsz <andrewrcdey@gmail.com>
2021-12-20 14:13:09 -0500

    cleaned up dialog system, added documentation
    New Additions
    - Added missing `DialogOptions#Ok` dialog option
    
    Breaking Changes
    - `DialogUtil#showConfirmationDialog` now requires a `DialogOptions` parameter for improved confirmation usability
    - `DialogUtil#showOptionDialog` no longer needs `DialogOptions` specified -- `DialogOptions#Default` is used in its place as it tends to serve little purpose otherwise
    
    Other Changes
    - Changed `DialogUtil` to use `DialogMessageTypes` rather than `JOptionPane` directly

--- src/main/java/tech/fastj/graphics/dialog/DialogConfig.java
+++ src/main/java/tech/fastj/graphics/dialog/DialogConfig.java
@@ -3,7 +3,14 @@ package tech.fastj.graphics.dialog;
 import java.awt.Component;
 
 import javax.swing.Icon;
+import javax.swing.JPanel;
 
+/**
+ * Configuration class for creating {@link DialogUtil dialogs}.
+ *
+ * @author Andrew Dey
+ * @since 1.6.0
+ */
 public class DialogConfig {
 
     public static final Component DefaultParentComponent = null;
@@ -23,22 +30,49 @@ public class DialogConfig {
         this.icon = icon;
     }
 
+    /**
+     * Grabs an instance of a {@link DialogConfigBuilder} to use for creating a dialog.
+     *
+     * @return The {@link DialogConfigBuilder} instance.
+     */
     public static DialogConfigBuilder create() {
         return new DialogConfigBuilder();
     }
 
+    /**
+     * The Swing-based parent of the dialog to create.
+     *
+     * @return The dialog parent.
+     */
     public Component dialogParent() {
         return parentComponent;
     }
 
+    /**
+     * The {@code String} representing the title of the dialog.
+     *
+     * @return The title {@code String}.
+     */
     public String title() {
         return title;
     }
 
+    /**
+     * The prompt for the dialog. This can be almost anything -- a simple {@code int}, a {@code String}, even a {@link
+     * JPanel} containing a bunch of {@code Component}s for custom dialog content.
+     *
+     * @return The prompt {@code Object}.
+     */
     public Object prompt() {
         return prompt;
     }
 
+    /**
+     * The {@link Icon} used for the dialog. This is often provided by the dialog's {@link DialogMessageTypes message
+     * type}.
+     *
+     * @return The {@link Icon} instance.
+     */
     public Icon icon() {
         return icon;
     }
--- src/main/java/tech/fastj/graphics/dialog/DialogUtil.java
+++ src/main/java/tech/fastj/graphics/dialog/DialogUtil.java
@@ -1,42 +1,83 @@
 package tech.fastj.graphics.dialog;
 
-import javax.swing.JOptionPane;
+import java.awt.Component;
 
+import javax.swing.Icon;
+import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
+
+/**
+ * Convenience methods, combined with {@link DialogConfig}, to create simple Swing-style dialogs.
+ * <p>
+ * When working with Swing components directly, it is highly recommended to use {@link
+ * SwingUtilities#invokeLater(Runnable)}.
+ *
+ * @author Andrew Dey
+ * @since 1.6.0
+ */
 public class DialogUtil {
 
     private DialogUtil() {
         throw new IllegalStateException();
     }
 
+    /**
+     * Creates a {@link JOptionPane#showMessageDialog(Component, Object, String, int, Icon) dialog with a message for
+     * the user}.
+     * <p>
+     * This dialog uses the {@link DialogMessageTypes#Information information} message type.
+     *
+     * @param dialogConfig The dialog's configuration style.
+     */
     public static void showMessageDialog(DialogConfig dialogConfig) {
         JOptionPane.showMessageDialog(
                 dialogConfig.dialogParent(),
                 dialogConfig.prompt(),
                 dialogConfig.title(),
-                JOptionPane.INFORMATION_MESSAGE,
+                DialogMessageTypes.Information.jOptionMessageType,
                 dialogConfig.icon()
         );
     }
 
+    /**
+     * Creates a {@link JOptionPane#showInputDialog(Component, Object, String, int, Icon, Object[], Object) dialog with
+     * a simple text box for the user to input to}.
+     * <p>
+     * This dialog uses the {@link DialogMessageTypes#Question question} message type.
+     *
+     * @param dialogConfig The dialog's configuration style.
+     * @return The string of input from the user.
+     */
     public static String showInputDialog(DialogConfig dialogConfig) {
         return (String) JOptionPane.showInputDialog(
                 dialogConfig.dialogParent(),
                 dialogConfig.prompt(),
                 dialogConfig.title(),
-                JOptionPane.QUESTION_MESSAGE,
+                DialogMessageTypes.Question.jOptionMessageType,
                 dialogConfig.icon(),
                 null,
                 null
         );
     }
 
-    public static boolean showConfirmationDialog(DialogConfig dialogConfig) {
+    /**
+     * Creates a {@link JOptionPane#showConfirmDialog(Component, Object, String, int, int, Icon) dialog with a set of
+     * buttons to confirm or deny a specified action}.
+     * <p>
+     * This dialog uses the {@link DialogMessageTypes#Question question} message type.
+     *
+     * @param dialogConfig  The dialog's configuration style.
+     * @param dialogOptions The types of options to provide. In most multi-option cases, {@link
+     *                      DialogOptions#YesNoCancel} should suffice.
+     * @return Whether the user confirmed ({@code true}) or denied ({@code false}).
+     */
+    public static boolean showConfirmationDialog(DialogConfig dialogConfig, DialogOptions dialogOptions) {
         int result = JOptionPane.showConfirmDialog(
                 dialogConfig.dialogParent(),
                 dialogConfig.prompt(),
                 dialogConfig.title(),
-                JOptionPane.YES_NO_CANCEL_OPTION,
-                JOptionPane.QUESTION_MESSAGE,
+                dialogOptions.jOption,
+                DialogMessageTypes.Question.jOptionMessageType,
                 dialogConfig.icon()
         );
 
@@ -51,12 +92,23 @@ public class DialogUtil {
         }
     }
 
-    public static int showOptionDialog(DialogConfig dialogConfig, DialogOptions dialogOption, DialogMessageTypes dialogMessageType, Object[] options, Object initialOption) {
+    /**
+     * Creates a {@link JOptionPane#showOptionDialog(Component, Object, String, int, int, Icon, Object[], Object) dialog
+     * with a set of buttons to choose from, as options}.
+     *
+     * @param dialogConfig      The dialog's configuration style.
+     * @param dialogMessageType The type of the dialog message.
+     * @param options           The options for the user to pick from.
+     * @param initialOption     The initial highlighted option.
+     * @return The index of the option the user chose. If the user exited the dialog or otherwise did not pick an
+     * option, {@code -1} is the returned value.
+     */
+    public static int showOptionDialog(DialogConfig dialogConfig, DialogMessageTypes dialogMessageType, Object[] options, Object initialOption) {
         return JOptionPane.showOptionDialog(
                 dialogConfig.dialogParent(),
                 dialogConfig.prompt(),
                 dialogConfig.title(),
-                dialogOption.jOption,
+                DialogOptions.Default.jOption,
                 dialogMessageType.jOptionMessageType,
                 dialogConfig.icon(),
                 options,
