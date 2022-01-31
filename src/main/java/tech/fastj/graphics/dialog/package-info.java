3dbe6433c96818c7e9df00387a7ebaa271536708
lucasstarsz <andrewrcdey@gmail.com>
2022-01-31 11:23:11 -0500

    updated package info documentation

---
+++ src/main/java/tech/fastj/graphics/dialog/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Rudimentary dialog system.
+ * <p>
+ * If more control is needed, look to {@link javax.swing.JOptionPane} and {@link javax.swing.JDialog}.
+ */
+package tech.fastj.graphics.dialog;
