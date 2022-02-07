2dfe385e1cfd1e6817b5f772a1ecc849afc1847f
YeffyCodeGit <aditchakraborty2009@gmail.com>
2022-02-07 15:12:42 +0600

    add addScenes function

--- src/main/java/tech/fastj/systems/control/SceneManager.java
+++ src/main/java/tech/fastj/systems/control/SceneManager.java
@@ -163,6 +163,29 @@ public abstract class SceneManager implements LogicManager {
     }
 
     /**
+     * Adds all the specified scenes into the logic manager.
+     * 
+     * @param scenes All the Scene objects to be added.
+     */
+    public void addScenes(Scene... scenes) {
+        for (Scene scene : scenes) {
+            addScene(scene);
+        }
+    }
+
+    /**
+     * Adds all the specified scenes into the logic manager.
+     * 
+     * @param scenes The list of Scene objects to be added.
+     */
+    public void addScenes(List<Scene> scenes) {
+        for (Scene scene : scenes) {
+            addScene(scene);
+        }
+    }
+
+
+    /**
      * Removes the specified scene from the logic manager.
      *
      * @param scene The Scene object to be removed.
