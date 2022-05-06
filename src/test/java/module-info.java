b9b62225eb170fe42a88553763ccafc154b4d0b2
lucasstarsz <andrewrcdey@gmail.com>
2022-05-06 17:27:12 -0400

    remove buggy input tests

--- src/test/java/module-info.java
+++ src/test/java/module-info.java
@@ -13,7 +13,6 @@ module fastj.library.test {
     opens unittest.testcases.graphics.util to org.junit.platform.commons;
 
     opens unittest.testcases.input.keyboard to org.junit.platform.commons;
-    opens unittest.testcases.input.mouse to org.junit.platform.commons;
 
     opens unittest.testcases.resources.models to org.junit.platform.commons;
 
