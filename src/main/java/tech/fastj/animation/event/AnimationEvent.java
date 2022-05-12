c36989292ccf9c64dd1d18b98289852d010e6133
lucasstarsz <andrewrcdey@gmail.com>
2022-05-12 01:16:43 -0400

    run spotlessApply

--- src/main/java/tech/fastj/animation/event/AnimationEvent.java
+++ src/main/java/tech/fastj/animation/event/AnimationEvent.java
@@ -1,8 +1,8 @@
 package tech.fastj.animation.event;
 
+import tech.fastj.gameloop.event.GameEvent;
 import tech.fastj.animation.Animated;
 import tech.fastj.animation.AnimationData;
-import tech.fastj.gameloop.event.GameEvent;
 
 public interface AnimationEvent<TD extends AnimationData, T extends Animated<TD>> extends GameEvent {
 
