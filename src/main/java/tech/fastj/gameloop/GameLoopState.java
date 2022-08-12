2fc252e76691845e683d425d4d12d37a42fb533c
lucasstarsz <andrewrcdey@gmail.com>
2022-08-12 04:46:53 -0400

    modify and add missing docs to tech.fastj.gameloop, rename game loop clear to better specify event system clearing

--- src/main/java/tech/fastj/gameloop/GameLoopState.java
+++ src/main/java/tech/fastj/gameloop/GameLoopState.java
@@ -4,8 +4,23 @@ import java.util.Objects;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
+/**
+ * State to be run in {@link GameLoop the game loop} at its given {@link #getCoreLoopState() core loop state}.
+ * <p>
+ * A game loop state defines logic that should be run whenever the {@link GameLoop game loop} reaches it during runtime iteration. The order
+ * for game loop states and when they are to be run is based on several factors:
+ * <ol>
+ *     <li>The {@link #getCoreLoopState() core loop state}</li>
+ *     <li>Whether the state has {@link #hasEnginePriority() engine priority}</li>
+ *     <li>The {@link #getPriority() general priority}</li>
+ * </ol>
+ *
+ * @author Andrew Dey
+ * @since 1.7.0
+ */
 public class GameLoopState implements Comparable<GameLoopState>, Consumer<Float> {
 
+    /** The default engine priority for game loop states. */
     public static final boolean DefaultEnginePriority = false;
 
     private final CoreLoopState coreLoopState;
@@ -15,10 +30,25 @@ public class GameLoopState implements Comparable<GameLoopState>, Consumer<Float>
 
     private boolean isActive;
 
+    /**
+     * Constructs a game loop state, with a {@link #DefaultEnginePriority default engine priority}.
+     *
+     * @param coreLoopState   The core loop state defining when the game loop state should be run.
+     * @param priority        The general priority defining when the game loop state should be run, within its core loop state.
+     * @param loopStateAction The action to run for the loop state.
+     */
     public GameLoopState(CoreLoopState coreLoopState, int priority, BiConsumer<GameLoopState, Float> loopStateAction) {
         this(coreLoopState, DefaultEnginePriority, priority, loopStateAction);
     }
 
+    /**
+     * Constructs a game loop state.
+     *
+     * @param coreLoopState   The core loop state defining when the game loop state should be run.
+     * @param enginePriority  Whether the game loop state has engine priority.
+     * @param priority        The general priority defining when the game loop state should be run, within its core loop state.
+     * @param loopStateAction The action to run for the loop state.
+     */
     public GameLoopState(CoreLoopState coreLoopState, boolean enginePriority, int priority, BiConsumer<GameLoopState, Float> loopStateAction) {
         this.coreLoopState = Objects.requireNonNull(coreLoopState);
         this.hasEnginePriority = enginePriority;
@@ -26,26 +56,40 @@ public class GameLoopState implements Comparable<GameLoopState>, Consumer<Float>
         this.loopStateAction = Objects.requireNonNull(loopStateAction);
     }
 
+    /** {@return the core loop state defining when in the {@link GameLoop game loop} the game loop state should be run} */
     public CoreLoopState getCoreLoopState() {
         return coreLoopState;
     }
 
+    /** {@return the general priority defining specifically when in the {@link GameLoop game loop} it should be run} */
     public int getPriority() {
         return subStatePriority;
     }
 
+    /** {@return whether the game loop state has engine priority, affecting when in the {@link GameLoop game loop} it should be run} */
     public boolean hasEnginePriority() {
         return hasEnginePriority;
     }
 
+    /**
+     * {@return whether the game loop state is actively being used}.
+     * <p>
+     * This value is currently not used. Check back in a later engine version for improved support.
+     */
     public boolean isActive() {
         return isActive;
     }
 
+    /** {@return the action the game loop state will run when used} */
     public BiConsumer<GameLoopState, Float> getLoopStateAction() {
         return loopStateAction;
     }
 
+    /**
+     * Internal method used to run the loop state.
+     *
+     * @param deltaTime delta (or fixed delta) time from the {@link GameLoop game loop}.
+     */
     @Override
     public synchronized void accept(Float deltaTime) {
         isActive = true;
@@ -80,8 +124,8 @@ public class GameLoopState implements Comparable<GameLoopState>, Consumer<Float>
         }
         GameLoopState gameLoopState = (GameLoopState) other;
         return subStatePriority == gameLoopState.subStatePriority
-                && hasEnginePriority == gameLoopState.hasEnginePriority
-                && coreLoopState == gameLoopState.coreLoopState;
+            && hasEnginePriority == gameLoopState.hasEnginePriority
+            && coreLoopState == gameLoopState.coreLoopState;
     }
 
     @Override
@@ -92,9 +136,9 @@ public class GameLoopState implements Comparable<GameLoopState>, Consumer<Float>
     @Override
     public String toString() {
         return "GameLoopState{" +
-                "baseLoopState=" + coreLoopState +
-                ", hasEnginePriority=" + hasEnginePriority +
-                ", subStatePriority=" + subStatePriority +
-                '}';
+            "baseLoopState=" + coreLoopState +
+            ", hasEnginePriority=" + hasEnginePriority +
+            ", subStatePriority=" + subStatePriority +
+            '}';
     }
 }
---
+++ src/main/java/tech/fastj/gameloop/package-info.java
@@ -0,0 +1,17 @@
+/**
+ * {@link tech.fastj.gameloop.GameLoop Game loop} with highly customizable {@link tech.fastj.gameloop.GameLoopState loop states}, and an
+ * {@link tech.fastj.gameloop.event event system}.
+ * <p>
+ * Events integrated with FastJ:
+ * <ul>
+ *     <li>{@link tech.fastj.animation.sprite.SpriteAnimationEngine Sprite Animation}</li>
+ *     <li>{@link tech.fastj.graphics.display.SimpleDisplay Display}</li>
+ *     <li>{@link tech.fastj.systems.audio.Audio Audio}</li>
+ *     <li>
+ *         {@link tech.fastj.input.keyboard.KeyboardActionListener Keyboard} and {@link tech.fastj.input.mouse.MouseActionListener Mouse}
+ *         Input
+ *     </li>
+ *     <li>{@link tech.fastj.engine.FastJEngine#runLater(java.lang.Runnable) Delayed action execution}</li>
+ * </ul>
+ */
+package tech.fastj.gameloop;
