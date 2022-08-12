64d7ace8d366720aa0a9d1e0358245ee55150184
lucasstarsz <andrewrcdey@gmail.com>
2022-08-12 05:02:27 -0400

    add more missing docs to tech.fastj.engine/engine.config

--- src/main/java/tech/fastj/engine/config/EngineConfig.java
+++ src/main/java/tech/fastj/engine/config/EngineConfig.java
@@ -2,6 +2,8 @@ package tech.fastj.engine.config;
 
 import tech.fastj.engine.FastJEngine;
 import tech.fastj.engine.HWAccel;
+import tech.fastj.graphics.display.Display;
+import tech.fastj.graphics.display.FastJCanvas;
 import tech.fastj.logging.LogLevel;
 import tech.fastj.math.Point;
 import tech.fastj.systems.control.LogicManager;
@@ -19,8 +21,20 @@ import java.util.Objects;
 public record EngineConfig(int targetFPS, int targetUPS, Point windowResolution, Point canvasResolution, HWAccel hardwareAcceleration,
                            ExceptionAction exceptionAction, LogLevel logLevel) {
 
+    /** Engine config with default values. */
     public static final EngineConfig Default = EngineConfig.create().build();
 
+    /**
+     * Constructs an engine config.
+     *
+     * @param targetFPS            The target frames per second.
+     * @param targetUPS            The target updates per second.
+     * @param windowResolution     The starting {@link Display window} resolution.
+     * @param canvasResolution     The starting {@link FastJCanvas canvas} resolution.
+     * @param hardwareAcceleration The type of {@link HWAccel hardware acceleration} used.
+     * @param exceptionAction      The type of {@link ExceptionAction action} to take for engine exceptions.
+     * @param logLevel             The {@link LogLevel log level} for the engine to use.
+     */
     public EngineConfig(int targetFPS, int targetUPS, Point windowResolution, Point canvasResolution, HWAccel hardwareAcceleration,
                         ExceptionAction exceptionAction, LogLevel logLevel) {
         this.targetFPS = targetFPS > 0 ? targetFPS : FastJEngine.DefaultFPS;
@@ -32,6 +46,7 @@ public record EngineConfig(int targetFPS, int targetUPS, Point windowResolution,
         this.logLevel = Objects.requireNonNullElse(logLevel, FastJEngine.DefaultLogLevel);
     }
 
+    /** {@return the builder class for creating an engine config} */
     public static EngineConfigBuilder create() {
         return new EngineConfigBuilder();
     }
--- src/main/java/tech/fastj/engine/config/EngineConfigBuilder.java
+++ src/main/java/tech/fastj/engine/config/EngineConfigBuilder.java
@@ -2,6 +2,8 @@ package tech.fastj.engine.config;
 
 import tech.fastj.engine.FastJEngine;
 import tech.fastj.engine.HWAccel;
+import tech.fastj.graphics.display.Display;
+import tech.fastj.graphics.display.FastJCanvas;
 import tech.fastj.logging.LogLevel;
 import tech.fastj.math.Point;
 
@@ -25,41 +27,88 @@ public class EngineConfigBuilder {
     EngineConfigBuilder() {
     }
 
+    /**
+     * Sets the target frames per second to be used.
+     *
+     * @param targetFPS The target FPS.
+     * @return The builder instance for method chaining.
+     */
     public EngineConfigBuilder withTargetFPS(int targetFPS) {
         this.targetFPS = targetFPS;
         return this;
     }
 
+    /**
+     * Sets the target updates per second to be used.
+     *
+     * @param targetUPS The target UPS.
+     * @return The builder instance for method chaining.
+     */
     public EngineConfigBuilder withTargetUPS(int targetUPS) {
         this.targetUPS = targetUPS;
         return this;
     }
 
+    /**
+     * Sets the {@link Display window} resolution to be used.
+     *
+     * @param windowResolution Thw window resolution.
+     * @return The builder instance for method chaining.
+     */
     public EngineConfigBuilder withWindowResolution(Point windowResolution) {
         this.windowResolution = windowResolution.copy();
         return this;
     }
 
+    /**
+     * Sets the {@link FastJCanvas canvas} resolution to be used.
+     *
+     * @param canvasResolution Thw canvas resolution.
+     * @return The builder instance for method chaining.
+     */
     public EngineConfigBuilder withCanvasResolution(Point canvasResolution) {
         this.canvasResolution = canvasResolution.copy();
         return this;
     }
 
+    /**
+     * Sets the {@link HWAccel hardware acceleration} to be used.
+     *
+     * @param hardwareAcceleration The hardware acceleration.
+     * @return The builder instance for method chaining.
+     */
     public EngineConfigBuilder withHardwareAcceleration(HWAccel hardwareAcceleration) {
         this.hardwareAcceleration = hardwareAcceleration;
         return this;
     }
 
+    /**
+     * Sets the {@link ExceptionAction exception action} to be used.
+     *
+     * @param exceptionAction The exception action.
+     * @return The builder instance for method chaining.
+     */
     public EngineConfigBuilder withExceptionAction(ExceptionAction exceptionAction) {
         this.exceptionAction = exceptionAction;
         return this;
     }
 
+    /**
+     * Sets the {@link LogLevel log level} to be used.
+     *
+     * @param logLevel The logging level.
+     * @return The builder instance for method chaining.
+     */
     public EngineConfigBuilder withLogLevel(LogLevel logLevel) {
         this.logLevel = logLevel;
         return this;
     }
 
+    /**
+     * Constructs an {@link EngineConfig} instance from the builder's set values.
+     *
+     * @return The new {@link EngineConfig} instance.
+     */
     public EngineConfig build() {
         return new EngineConfig(
             targetFPS,
